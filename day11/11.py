a8ff86eaa23eb633761d4c9765b49ee5d491cae4
Zeffe Struyf <zeffe.struyf@gmail.com>
2023-12-14 18:23:19 +0400

    day 11 part 2

--- day11/11.py
+++ day11/11.py
@@ -2,18 +2,54 @@ from itertools import combinations
 from collections import defaultdict
 
 
-def check_cols(galaxy):
+def get_empty_cols(universe):
     empty_cols = []
-    row_length = len(galaxy[0])
+    row_length = len(universe[0])
 
     for i in range(row_length):
         galaxy_count = 0
-        for row in galaxy:
+        for row in universe:
             if row[i] == '#':
                 galaxy_count += 1
 
         if galaxy_count == 0:
             empty_cols.append(i)
+
+    return empty_cols
+
+
+def get_empty_rows(galaxy):
+    empty_rows = []
+
+    for i, line in enumerate(galaxy):
+        galaxy_count = 0
+        for c in line:
+            if c == '#':
+                galaxy_count += 1
+
+        if galaxy_count == 0:
+            empty_rows.append(i)
+
+    return empty_rows
+
+
+def calculate_empty_cols_crossed(galaxy, empty_cols):
+    return len([x for x in empty_cols if galaxy[1] > x])
+
+
+def calculate_empty_rows_crossed(galaxy, empty_rows):
+    return len([x for x in empty_rows if galaxy[0] > x])
+
+
+def get_new_position(galaxy, empty_rows, empty_cols):
+    cols_crossed = calculate_empty_cols_crossed(galaxy, empty_cols)
+    rows_crossed = calculate_empty_rows_crossed(galaxy, empty_rows)
+
+    return galaxy[0] + (999999 * rows_crossed), galaxy[1] + (999999 * cols_crossed)
+
+
+def check_cols(galaxy):
+    empty_cols = get_empty_cols(galaxy)
 
     for i, row in enumerate(galaxy):
         cols = empty_cols[0:]
@@ -54,37 +90,63 @@ def check_rows(galaxy):
     return expanded_galaxy
 
 
+def get_galaxies(universe):
+    galaxies = []
+    for r, line in enumerate(universe):
+        for c, thing in enumerate(line):
+            if thing == '#':
+                galaxies.append((r, c))
+
+    return galaxies
+
+
 def def_value():
     return 0
+
+
+def get_shortest_distance(pair):
+    return abs(pair[0][0] - pair [1][0]) + abs(pair[0][1] - pair[1][1])
+
+
+def part1(lines):
+    expanded_galaxy = check_cols(lines)
+    expanded_galaxy = check_rows(expanded_galaxy)
+
+    galaxies = get_galaxies(expanded_galaxy)
+
+    pairs = list(combinations(galaxies, 2))
+    print(len(pairs))
+
+    total = 0
+    for pair in pairs:
+        total += get_shortest_distance(pair)
+
+    print(f'The sum of all shortest lengths is {total}')
+
+
+def part2(lines):
+    galaxies = get_galaxies(lines)
+    empty_cols = get_empty_cols(lines)
+    empty_rows = get_empty_rows(lines)
+
+    for i, galaxy in enumerate(galaxies):
+        galaxies[i] = get_new_position(galaxy, empty_rows, empty_cols)
+
+    pairs = list(combinations(galaxies, 2))
+    print(len(pairs))
+
+    total = 0
+    for pair in pairs:
+        total += get_shortest_distance(pair)
+
+    print(f'The sum of distances in the larger universe is {total}')
 
 
 def main(filename):
     data = open(filename).read()
     lines = data.split('\n')
-
-    expanded_galaxy = check_cols(lines)
-    expanded_galaxy = check_rows(expanded_galaxy)
-
-    print(expanded_galaxy)
-
-    galaxies = []
-    for r, line in enumerate(expanded_galaxy):
-        for c, thing in enumerate(line):
-            if thing == '#':
-                galaxies.append((r, c))
-
-    pairs = list(combinations(galaxies, 2))
-
-    print(galaxies)
-
-    total = 0
-    for pair in pairs:
-        shortest_distance = abs(pair[0][0] - pair[1][0]) + abs(pair[0][1] - pair[1][1])
-
-        print(f'The shortest distance between {pair} is {shortest_distance}')
-        total += shortest_distance
-
-    print(total)
+    part1(lines[0:])
+    part2(lines[0:])
 
 
 if __name__ == '__main__':
